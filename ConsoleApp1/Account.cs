27a316f64ae42180b222688c6753e3816a4bc3dc
Баринов Марсель Алексеевич <211902@mck-ktits.ru>
2021-09-07 08:44:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Account
+    {
+        private double balance;
+        private int accountSeller;
+        private int accountGetter;
+        public double Balance { get; set; }
+        
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;  
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account();
+            Account acc2 = new Account();
+            acc.Balance = 2000;
+            acc2.Balance = 7000;
+            Operations.ShowBalance(acc);
+            Operations.ShowBalance(acc2);
+
+            Operations.Transaction(acc, acc2, 500);
+            
+            Operations.Witdraw(acc, 600);
+
+            
+        }
+    }
+}
